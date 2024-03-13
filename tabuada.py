1780c8d22bab12407381597ac8e2420eaf1cb88e
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 10:49:24 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+print('#' * 40)
+print("Tabuada do Python")
+tabuada = int(input("Entre com o numero da tabuda?\n"))
+print('#' * 40)
+
+print(f'Tabuada dos {tabuada}:')
+for i in range(11):
+  print(f'{tabuada} x {i} = {tabuada * i}')
+print('#' * 40)
+print('Fim do Programa!')
