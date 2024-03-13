f7e38d12a2ecacda6c684bdd506bec6bccbe8d6b
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 10:49:52 -0300

    Create time_venceu.py

---
+++ time_venceu.py
@@ -0,0 +1,16 @@
+print('#' * 40)
+entrada = int(input("Seu time venceu a última partida?\n"))
+print('1 - sim')
+print('2 - não')
+print('#' * 40)
+
+if entrada == 1:
+    print("Muito bem!")
+elif entrada == 2:
+    print("Muito bem!")
+else:
+    print("Valor inválido")
+
+print('Fim do programa')
+
+# https://www.linkedin.com/pulse/switch-case-est%C3%A1-vindo-para-o-python-voc%C3%AA-deve-us%C3%A1-lo-cardoso
