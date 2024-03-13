918c7f34f7ad8bec1341a77bf91a2852dbb4bd29
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 13:16:59 -0300

    Create ordenar-lista.py

---
+++ ordenar-lista.py
@@ -0,0 +1,10 @@
+lista = [
+  
+"Maria", "Carlos", "Jose", "João", "Lindomar", "Rayan","Welliton", "Vinicius", "Jilian", "Lucas", "Gabriel", "Gregorio", "Helio","Nóe", "Matheus", "Jesus", "Londrina", "Rio de Janeiro", "Lindoia", "Dubai", "Musse", "Picanha",
+"Lentilha"
+"Comoda", "Janela", "Armario", "Empresa", "Escola", "Armazem"
+]
+
+ordem = sorted(lista)
+
+print(ordem)
