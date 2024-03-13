1ef7f51620aec599306c029cc3c248ea7e838c6e
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 10:46:25 -0300

    Create for_com_python.py

---
+++ for_com_python.py
@@ -0,0 +1,14 @@
+tupla_alunos = ('Joao', 'Maria', 'Kaio')
+# modelo 001
+for i, aluno in enumerate(tupla_alunos):
+  print(i, aluno)
+
+# modelo 002
+  
+for aluno in tupla_alunos:
+   print(aluno)
+   
+# modelo 003
+
+for num in range(10):
+   print(num)
