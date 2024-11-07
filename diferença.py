ea6c439c4d7be1ac5d15d66c22bacd8b54498b31
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-11-07 11:42:29 -0300

    cisco

---
+++ diferença.py
@@ -0,0 +1,9 @@
+
+valores = [5,2,9,1]
+diferenca = valores[0]
+
+for valor in valores[1:]:
+    diferenca -= valor
+    
+print(diferenca)
+    
---
+++ produto.py
@@ -0,0 +1,9 @@
+
+numeros = [1,2,3,4]
+produto = 1
+
+for numero in numeros:
+    produto *= numero
+    
+print(produto)
+    
