a4b1947b6917eb4351f273ac096ca7e92349f501
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-02-27 19:54:26 -0300

    Create ordenar-lista-dados.py

---
+++ ordenar-lista-dados.py
@@ -0,0 +1,42 @@
+# Criar uma lista
+minha_lista = [5, 2, 9, 1, 7, 3]
+
+# Imprimir a lista original
+print("Lista original:")
+print(minha_lista)
+
+# Criar uma nova lista ordenada em ordem crescente
+lista_ordenada_crescente = sorted(minha_lista)
+
+# Imprimir a lista ordenada em ordem crescente
+print("\nLista ordenada em ordem crescente:")
+print(lista_ordenada_crescente)
+
+# Criar uma nova lista ordenada em ordem decrescente
+lista_ordenada_decrescente = sorted(minha_lista, reverse=True)
+
+# Imprimir a lista ordenada em ordem decrescente
+print("\nLista ordenada em ordem decrescente:")
+print(lista_ordenada_decrescente)
+
+
+# Criar uma lista e ordenar com sort
+minha_lista = [5, 2, 9, 1, 7, 3]
+
+# Imprimir a lista original
+print("Lista original:")
+print(minha_lista)
+
+# Ordenar a lista em ordem crescente
+minha_lista.sort()
+
+# Imprimir a lista ordenada em ordem crescente
+print("\nLista ordenada em ordem crescente:")
+print(minha_lista)
+
+# Ordenar a lista em ordem decrescente
+minha_lista.sort(reverse=True)
+
+# Imprimir a lista ordenada em ordem decrescente
+print("\nLista ordenada em ordem decrescente:")
+print(minha_lista)
